2aeabf26329198f5d42797f6d39123a45dcbc13f
Sanjay Karki <sanjaykarki@live.com>
2020-08-08 16:43:21 +0200

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_app/widgets/destination_carousel.dart';
+import 'package:travel_app/widgets/hotel_carousel.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  // Selects the icons from Font Awesome
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.car,
+    FontAwesomeIcons.bicycle,
+  ];
+
+  Widget _buildIcons(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFEB4C1C4),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding:
+              EdgeInsets.symmetric(vertical: 30.0), // Adds the padding on top
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 20.0,
+                  right: 120.0), //Adds the padding on left and right
+              child: Text(
+                'What would you like to find?',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20.0), // For padding between text and Buttons
+            // Start of Buttons Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((MapEntry map) => _buildIcons(map.key))
+                  .toList(),
+            ),
+            //End of Buttons row
+            SizedBox(height: 20.0), // Add the padding on "top Destination"
+            DestinationCarousel(), // Imported the Destination Carousel from widgets/destination_carousel
+            SizedBox(height: 20.0), // Add the padding on "top Destination"
+            HotelCarousel(),
+          ],
+        ),
+      ),
+    );
+  }
+}
